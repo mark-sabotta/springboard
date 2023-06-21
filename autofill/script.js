06086b4a00e8862a6524604f511568a73f155bb3
Mark Sabotta <mark.sabotta@gmail.com>
2023-06-21 11:37:37 -0600

    objectifying connect 4

--- autofill/script.js
+++ autofill/script.js
@@ -3,8 +3,6 @@ const suggestions = document.querySelector('.suggestions ul');
 
 const fruits = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
-let boldMap = new Map();
-
 function search(str) {
 	let results = [];
 	let lowerCaseString = str.toLowerCase();
@@ -27,18 +25,13 @@ function searchHandler(e) {
 
 function showSuggestions(results, inputVal) {
 	suggestions.innerHTML = "";
+	let lowerInputVal = inputVal.toLowerCase();
 	results.forEach((fruit) => {
+		let lowerFruit = fruit.toLowerCase();
 		let newSuggestion = document.createElement('li');
-		let splitPoint = fruit.indexOf(inputVal); //results is sure to contain matches
-		let boldHighlight = ''; //need to initialize the bolded value here to avoid having "let" twice
+		let splitPoint = lowerFruit.indexOf(lowerInputVal); //results is sure to contain matches
+		let boldHighlight = fruit.slice(0, splitPoint) + "<strong>" + fruit.slice(splitPoint, splitPoint + inputVal.length) + "</strong>" + fruit.slice(splitPoint + inputVal.length);
 
-		//if the index of 
-		if(splitPoint != -1){
-			boldHighlight = fruit.slice(0, splitPoint) + "<strong>" + inputVal + "</strong>" + fruit.slice(splitPoint + inputVal.length);
-		}else{
-			boldHighlight = "<strong>" + fruit.slice(0, inputVal.length) + "</strong>" + fruit.slice(inputVal.length);
-		}
-		boldMap.set(boldHighlight, fruit);
 		newSuggestion.innerHTML = boldHighlight;
 		suggestions.appendChild(newSuggestion);
 	});
@@ -48,7 +41,7 @@ function showSuggestions(results, inputVal) {
 }
 
 function useSuggestion(e) {
-	input.value = boldMap.get(e.target.innerHTML);
+	input.value = boldMap.get(e.target.innerText);
 	suggestions.innerHTML = "";
 	suggestions.classList.remove("has-suggestions");
 }
--- connect-four-oo/connect4.js
+++ connect-four-oo/connect4.js
@@ -5,23 +5,45 @@
  * board fills (tie)
  */
 class Game{
-  constructor(WIDTH, HEIGHT){
+  constructor(HEIGHT, WIDTH){
     this.WIDTH = WIDTH;
     this.HEIGHT = HEIGHT;
     this.board = [];
+    this.makeBoard();
+    this.makeHtmlBoard();
+    this.playerList = this.readyPlayers();
+    this.currPlayer = this.playerList[0];
+    this.gameOver = 0;
+  }
+  readyPlayers(){
+    if(document.querySelector("#p1Color").value != document.querySelector("#p2Color").value){
+      return [{
+        name: document.querySelector("#p1Name").value,
+        color: document.querySelector("#p1Color").value
+        }, {
+        name: document.querySelector("#p2Name").value,
+        color: document.querySelector("#p2Color").value
+      }];
+    }else{
+      alert("Player colors must not match.");
+    }
   }
   makeBoard() {
+    this.board = [];
     for (let y = 0; y < this.HEIGHT; y++) {
       this.board.push(Array.from({ length: this.WIDTH }));
     }
   }
   makeHtmlBoard() {
     const board = document.getElementById('board');
+    board.innerHTML = '';
   
     // make column tops (clickable area for adding a piece to that column)
     const top = document.createElement('tr');
     top.setAttribute('id', 'column-top');
-    top.addEventListener('click', handleClick);
+
+    this.handleGameClick = this.handleClick.bind(this);
+    top.addEventListener('click', this.handleGameClick);
   
     for (let x = 0; x < this.WIDTH; x++) {
       const headCell = document.createElement('td');
@@ -35,7 +57,7 @@ class Game{
     for (let y = 0; y < this.HEIGHT; y++) {
       const row = document.createElement('tr');
   
-      for (let x = 0; x < WIDTH; x++) {
+      for (let x = 0; x < this.WIDTH; x++) {
         const cell = document.createElement('td');
         cell.setAttribute('id', `${y}-${x}`);
         row.append(cell);
@@ -46,7 +68,7 @@ class Game{
   }
   findSpotForCol(x) {
     for (let y = this.HEIGHT - 1; y >= 0; y--) {
-      if (!board[y][x]) {
+      if (!this.board[y][x]) {
         return y;
       }
     }
@@ -55,7 +77,7 @@ class Game{
   placeInTable(y, x) {
     const piece = document.createElement('div');
     piece.classList.add('piece');
-    piece.classList.add(`p${currPlayer}`);
+    piece.style.backgroundColor = this.currPlayer.color;
     piece.style.top = -50 * (y + 2);
   
     const spot = document.getElementById(`${y}-${x}`);
@@ -63,93 +85,74 @@ class Game{
   }
   endGame(msg) {
     alert(msg);
+    this.gameOver = 1;
   }
-}
+  checkForWin() {
+    const _win = cells => 
+        cells.every(
+        ([y, x]) =>
+          y >= 0 &&
+          y < this.HEIGHT &&
+          x >= 0 &&
+          x < this.WIDTH &&
+          this.board[y][x] === this.currPlayer.color
+      );
 
-let currPlayer = 1; // active player: 1 or 2
- // array of rows, each row is array of cells  (board[y][x])
-
-/** makeBoard: create in-JS board structure:
- *   board = array of rows, each row is array of cells  (board[y][x])
- */
-
-/** makeHtmlBoard: make HTML table and row of column tops. */
-
-
-/** findSpotForCol: given column x, return top empty y (null if filled) */
-
-
-/** placeInTable: update DOM to place piece into HTML table of board */
-
-
-/** endGame: announce game end */
-
-
-
-/** handleClick: handle click of column top to play piece */
-
-function handleClick(evt) {
-  // get x from ID of clicked cell
-  const x = +evt.target.id;
-
-  // get next spot in column (if none, ignore click)
-  const y = findSpotForCol(x);
-  if (y === null) {
-    return;
-  }
-
-  // place piece in board and add to HTML table
-  board[y][x] = currPlayer;
-  placeInTable(y, x);
   
-  // check for win
-  if (checkForWin()) {
-    return endGame(`Player ${currPlayer} won!`);
-  }
+    for (let y = 0; y < this.HEIGHT; y++) {
+      for (let x = 0; x < this.WIDTH; x++) {
+        // get "check list" of 4 cells (starting here) for each of the different
+        // ways to win
+        const horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
+        const vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];  
+        const diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
+        const diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
   
-  // check for tie
-  if (board.every(row => row.every(cell => cell))) {
-    return endGame('Tie!');
-  }
-    
-  // switch players
-  currPlayer = currPlayer === 1 ? 2 : 1;
-}
-
-/** checkForWin: check board cell-by-cell for "does a win start here?" */
-
-function checkForWin() {
-  function _win(cells) {
-    // Check four cells to see if they're all color of current player
-    //  - cells: list of four (y, x) cells
-    //  - returns true if all are legal coordinates & all match currPlayer
-
-    return cells.every(
-      ([y, x]) =>
-        y >= 0 &&
-        y < HEIGHT &&
-        x >= 0 &&
-        x < WIDTH &&
-        board[y][x] === currPlayer
-    );
-  }
-
-  for (let y = 0; y < HEIGHT; y++) {
-    for (let x = 0; x < WIDTH; x++) {
-      // get "check list" of 4 cells (starting here) for each of the different
-      // ways to win
-      const horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
-      const vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
-      const diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
-      const diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
-
-      // find winner (only checking each win-possibility as needed)
-      if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
-        return true;
+        // find winner (only checking each win-possibility as needed)
+        if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {//why no this. ?
+          return true;
+        }
       }
     }
   }
+  handleClick(evt) {
+    // get x from ID of clicked cell
+    const x = +evt.target.id;
+  
+    // get next spot in column (if none, ignore click)
+    const y = this.findSpotForCol(x);
+    if (y === null) {
+      return;
+    }
+  
+    // place piece in board and add to HTML table
+    this.board[y][x] = this.currPlayer.color;
+    this.placeInTable(y, x);
+    
+    // check for win
+    if (this.checkForWin()) {
+      return this.endGame(`${this.currPlayer.name} won!`);
+    }
+    
+    // check for tie
+    if (this.board.every(row => row.every(cell => cell))) {
+      return this.endGame('Tie!');
+    }
+      
+    // switch players
+    this.currPlayer = this.currPlayer === this.playerList[0] ? this.playerList[1] : this.playerList[0];
+  }
+  
+  /** checkForWin: check board cell-by-cell for "does a win start here?" */
+  
+  
 }
+let startBtn = document.querySelector('#gameStart');
+startBtn.addEventListener("submit", function (e){
+  e.preventDefault();
+  new Game(document.querySelector('#height').value, document.querySelector('#width').value);
+})
 
-makeBoard();
-makeHtmlBoard();
+
+
+
