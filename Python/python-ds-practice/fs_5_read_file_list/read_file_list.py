ccaf50f132a55ab39aa0df618a3111dfa33cd775
Mark Sabotta <mark.sabotta@gmail.com>
2023-07-02 14:48:32 -0600

    adding python ds fs 5

--- Python/python-ds-practice/fs_5_read_file_list/read_file_list.py
+++ Python/python-ds-practice/fs_5_read_file_list/read_file_list.py
@@ -16,6 +16,12 @@ def read_file_list(filename):
     It will raise an error if the file cannot be found.
     """
 
-    # hint: when you read lines of files, there will be a "newline"
-    # (end-of-line character) at the end of each line, and you want to
-    # strip that off before you print it. Do some research on that!
+
+
+    with open(filename) as f:
+        for line in f:
+            newline = line.strip()
+            print(f"-{newline}")
+
+
+print(read_file_list("/Users/puff/Documents/c0d3/Springboard/Python/python-ds-practice/fs_5_read_file_list/cats"))
